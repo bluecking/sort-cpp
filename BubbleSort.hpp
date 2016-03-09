2398b1c7394f81677841f174db456018e01bfcf4
Benjamin Lücking <bluecking@uos.de>
2016-03-09 14:58:48 +0100

    implement bubblesort

---
+++ BubbleSort.hpp
@@ -0,0 +1,38 @@
+/**
+ * BubbleSort.hpp
+ *
+ *  @date   09.03.16
+ *  @author Benjamin Lücking <bluecking@uos.de>
+ */
+
+#ifndef SORTING_ALGORITHMS_BUBBLESORT_HPP
+#define SORTING_ALGORITHMS_BUBBLESORT_HPP
+
+template<typename T>
+class BubbleSort
+{
+public:
+    static void sort(T* list, int length)
+    {
+        bool swapped;
+
+        do
+        {
+            swapped = false;
+
+            for (int i = 1; i < length; ++i)
+            {
+                if (list[i - 1] > list[i])
+                {
+                    int tmp = list[i - 1];
+                    list[i - 1] = list[i];
+                    list[i] = tmp;
+                    swapped = true;
+                }
+            }
+        }
+        while (swapped);
+    }
+};
+
+#endif //SORTING_ALGORITHMS_BUBBLESORT_HPP
---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "BubbleSort.hpp"
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char** argv)
+{
+    int list[] = {20, 8, 1, 6, 13, 11, 2, 18, 16, 15, 19, 3, 10, 9, 12, 14, 17, 7, 4, 5};
+    int length = sizeof(list) / sizeof(list[0]);
+
+    BubbleSort<int>::sort(list, length);
+
+    for (int i = 0; i < (sizeof(list) / sizeof(list[0])); ++i)
+    {
+        cout << list[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
